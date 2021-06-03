2ebcf331c275be2eedfa1a93c0d453a11300cfe2
VishakAmin <aminvishak@gmail.com>
2021-06-04 02:42:11 +0530

    Table Component Completed

--- spacex_dashboard/src/Compenent/BasicTable.js
+++ spacex_dashboard/src/Compenent/BasicTable.js
@@ -4,6 +4,8 @@ import Table from '@material-ui/core/Table';
 import Box from '@material-ui/core/Box';
 import TableBody from '@material-ui/core/TableBody';
 import TableFooter from '@material-ui/core/TableFooter';
+import Button from '@material-ui/core/Button';
+
 //import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
@@ -12,6 +14,11 @@ import Paper from '@material-ui/core/Paper';
 import Pagination from '@material-ui/lab/Pagination';
 import MuiTableCell from "@material-ui/core/TableCell";
 import moment from 'moment'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 const TableCell = withStyles({
@@ -44,6 +51,9 @@ const BasicTable = ({ launch, loader }) => {
     const classes = useStyles();
 
     const [page, setPage] = useState(1);
+    const [open, setOpen] = useState(false);
+
+
     const rowsPerPage = 12;
 
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, launch.length - (page - 1) * rowsPerPage);
@@ -53,6 +63,43 @@ const BasicTable = ({ launch, loader }) => {
     };
 
     console.log(page)
+
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = () => (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Let Google help apps determine location. This means sending anonymous location data to
+                    Google, even when no apps are running.
+          </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Disagree
+          </Button>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                    Agree
+          </Button>
+            </DialogActions>
+        </Dialog>
+
+    )
+
     return (
 
         <Box className={classes.box} pt="112px" pl="245px" pr="245px" pb="164px">
@@ -73,6 +120,7 @@ const BasicTable = ({ launch, loader }) => {
 
                     <TableBody>
 
+                        {/* https://codesandbox.io/s/jzbml?file=/src/MyDialog.js */}
                         {launch
                             .slice((page - 1) * rowsPerPage, (page - 1) * rowsPerPage + rowsPerPage)
                             .map((row, index) => (
@@ -84,12 +132,25 @@ const BasicTable = ({ launch, loader }) => {
                                     <TableCell align="left">{row.launch_site.site_name}</TableCell>
                                     <TableCell align="left">{row.mission_name}</TableCell>
                                     <TableCell align="left">{row.rocket.second_stage.payloads[0].orbit}</TableCell>
-                                    <TableCell align="left">{row.launch_success ? "Success" : "Failed"}</TableCell>
+                                    <TableCell align="left">
+                                        <Button
+                                            color="primary"
+                                            variant="outlined"
+                                            onClick={() => handleChange(row)}
+                                        >
+                                            {row.launch_success ? "Success" : "Failed"}
+                                        </Button>
+
+                                    </TableCell>
                                     <TableCell align="left">{row.rocket.rocket_name}</TableCell>
-
-
                                 </TableRow>
+
+
+
                             ))}
+
+
+
                         {emptyRows > 0 && (
                             <TableRow style={{ height: 53 * emptyRows }}>
                                 <TableCell colSpan={6} />
