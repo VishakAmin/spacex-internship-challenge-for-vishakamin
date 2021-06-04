a456f5664e4c567314f92be7d873f4453d650df3
VishakAmin <aminvishak@gmail.com>
2021-06-05 02:07:52 +0530

    Created Modal Component

--- spacex_dashboard/src/Compenent/BasicTable.js
+++ spacex_dashboard/src/Compenent/BasicTable.js
@@ -5,8 +5,7 @@ import Box from '@material-ui/core/Box';
 import TableBody from '@material-ui/core/TableBody';
 import TableFooter from '@material-ui/core/TableFooter';
 import Button from '@material-ui/core/Button';
-
-//import TableCell from '@material-ui/core/TableCell';
+import { createMuiTheme } from '@material-ui/core/styles';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
@@ -19,6 +18,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MyModal from './MyModal'
 
 
 const TableCell = withStyles({
@@ -30,28 +30,57 @@ const TableCell = withStyles({
 
 const useStyles = makeStyles({
 
+
     box: {
         width: "952px",
-        height: "676px"
+        height: "676px",
+
     },
 
     table: {
         maxWidth: 952,
         maxHeight: 676
     },
-});
-// pagination: {
-//     width: "240px",
-//     height: "40px",
-//     left: "957px",
 
-//     top: "880px",
-// }
+    success: {
+        padding: "4px 12px",
+        borderRadius: "20px",
+        width: "58px",
+        height: "21px",
+        fontFamily: "Helvetica Neue",
+        fontStyle: "normal",
+        fontWeight: "500",
+        fontSize: "12px",
+        lineHeight: "13px",
+        textAlign: "center",
+        color: "#03543F",
+        background: "#DEF7EC",
+        textTransform: "none"
+    },
+    failed: {
+        padding: "4px 12px",
+        borderRadius: "20px",
+        width: "58px",
+        height: "21px",
+        fontStyle: "normal",
+        fontFamily: "Helvetica Neue",
+        fontWeight: "500",
+        fontSize: "12px",
+        lineHeight: "13px",
+        textAlign: "center",
+        color: "#981B1C",
+        background: "#FDE2E1",
+        textTransform: "none"
+    },
+});
+
+
 const BasicTable = ({ launch, loader }) => {
     const classes = useStyles();
 
     const [page, setPage] = useState(1);
-    const [open, setOpen] = useState(false);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [rowToUpdate, setRowToUpdate] = useState([]);
 
 
     const rowsPerPage = 12;
@@ -60,58 +89,41 @@ const BasicTable = ({ launch, loader }) => {
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
+        console.log(page)
+
+
+    };
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+
+    };
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
     };
 
-    console.log(page)
-
-
-
-    const handleClickOpen = () => {
-        setOpen(true);
+    const updateDetails = (e, row) => {
+        setOpenDialog(true);
+        setRowToUpdate(row);
     };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
 
-    const handleChange = () => (
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Let Google help apps determine location. This means sending anonymous location data to
-                    Google, even when no apps are running.
-          </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                    Disagree
-          </Button>
-                <Button onClick={handleClose} color="primary" autoFocus>
-                    Agree
-          </Button>
-            </DialogActions>
-        </Dialog>
-
-    )
-
+    console.log(rowToUpdate)
     return (
 
         <Box className={classes.box} pt="112px" pl="245px" pr="245px" pb="164px">
+            <MyModal
+                open_d={openDialog}
+                handleOpenDialog={handleOpenDialog}
+                handleCloseDialog={handleCloseDialog}
+                row={rowToUpdate} />
             <TableContainer component={Paper} className={classes.box} >
                 <Table className={classes.table} aria-label="simple table" >
                     <TableHead style={{ backgroundColor: "#F4F5F7" }}>
                         <TableRow>
                             <TableCell align="center">No:</TableCell>
-                            <TableCell align="left">Mission</TableCell>
-
                             <TableCell align="left">Launched(UTC)</TableCell>
                             <TableCell align="left">Location</TableCell>
+                            <TableCell align="left">Mission</TableCell>
                             <TableCell align="left">Orbit</TableCell>
                             <TableCell align="left">Launch Status</TableCell>
                             <TableCell align="left">Rocket</TableCell>
@@ -133,13 +145,25 @@ const BasicTable = ({ launch, loader }) => {
                                     <TableCell align="left">{row.mission_name}</TableCell>
                                     <TableCell align="left">{row.rocket.second_stage.payloads[0].orbit}</TableCell>
                                     <TableCell align="left">
-                                        <Button
-                                            color="primary"
+                                        {row.launch_success ? <Button
+                                            className={classes.success}
                                             variant="outlined"
-                                            onClick={() => handleChange(row)}
+
+                                            onClick={(e) => updateDetails(e, row)}
                                         >
-                                            {row.launch_success ? "Success" : "Failed"}
+                                            Success
                                         </Button>
+                                            :
+                                            <Button
+                                                className={classes.failed}
+                                                variant="outlined"
+                                                color="secondary"
+                                                onClick={(e) => updateDetails(e, row)}
+                                            >
+                                                Failed
+                                            </Button>
+
+                                        }
 
                                     </TableCell>
                                     <TableCell align="left">{row.rocket.rocket_name}</TableCell>
@@ -149,14 +173,13 @@ const BasicTable = ({ launch, loader }) => {
 
                             ))}
 
-
-
                         {emptyRows > 0 && (
                             <TableRow style={{ height: 53 * emptyRows }}>
                                 <TableCell colSpan={6} />
                             </TableRow>
                         )}
                     </TableBody>
+
 
                 </Table>
             </TableContainer>
